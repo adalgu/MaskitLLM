f1845c0a1661a1c08c3f1101542b86bc8bbf771a
Gunn <adalgU@gmail.com>
2024-09-20 14:46:18 +0900

    first commit

--- st.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Create required directories
-mkdir -p src tests data docs notebooks models
-
-echo "Folders created successfully:"
-echo "src/, tests/, data/, docs/, notebooks/, models/"
